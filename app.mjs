d8e2899d3b2ae6758153a22d3ee8d0c4c5899e9f
muhammad-shazaib <shazaibirshad126@gamil.com>
2025-05-29 20:41:11 +0500

    first commit

---
+++ app.mjs
@@ -0,0 +1,17 @@
+
+for (let i = 0; i < 10; i++) {
+
+    console.log('sum text');
+};
+
+
+
+
+
+
+
+
+
+// loop kesy khyty hai ??
+// repeatition ke liye use hota hai.
+// loop ka matlab hota hai ke kisi cheaz ko bar bar karna.
